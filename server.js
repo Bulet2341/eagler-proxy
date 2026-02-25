df05d83b9cf753e09bc7701896675c242fc58c07
Bulet2341 <bulet2341@gmail.com>
2026-02-24 23:22:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const http = require('http');
 const WebSocket = require('ws');
-const url = require('url');
 
-const VPS_HOST = 'YOUR_VPS_IP'; // public IPv4 of your VPS
+const VPS_HOST = 'YOUR_VPS_PUBLIC_IP';
 const VPS_PORT = 8081;
 
 const server = http.createServer();
-
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws, req) {
-  // Connect to EaglerXServer on VPS
+wss.on('connection', (ws) => {
   const proxy = new WebSocket(`ws://${VPS_HOST}:${VPS_PORT}`);
 
-  // Forward messages both ways
   ws.on('message', (msg) => proxy.send(msg));
   proxy.on('message', (msg) => ws.send(msg));
 
@@ -21,6 +17,6 @@ wss.on('connection', function connection(ws, req) {
   proxy.on('close', () => ws.close());
 });
 
-server.listen(process.env.PORT || 8080, () => {
-  console.log('Reverse proxy running');
+server.listen(process.env.PORT || 443, () => {
+  console.log('Eaglercraft proxy running');
 });
